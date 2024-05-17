ac2b6dd2e70c22cadbecabe1e5a4c296b6b7446d
natalia evgenievna <110298180+missChicherina@users.noreply.github.com>
2024-05-17 21:01:03 +0300

    Добавлен пулл портов и комментарии. Завершено

--- LR5/server.py
+++ LR5/server.py
@@ -1,27 +1,49 @@
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 import socket
-import pickle
-import random
 
+def generate_key_pair():
+    key = RSA.generate(2048)
+    private_key = key.export_key()
+    public_key = key.publickey().export_key()
+    return private_key, public_key
 
-def generate_private_key():
-    return random.randint(2, 100)
+def encrypt_message(message, public_key):
+    recipient_key = RSA.import_key(public_key)
+    cipher_rsa = PKCS1_OAEP.new(recipient_key)
+    encrypted_message = cipher_rsa.encrypt(message)
+    return encrypted_message
 
-def generate_public_key(g, p, private_key):
-    return (g ** private_key) % p
+def decrypt_message(encrypted_message, private_key):
+    key = RSA.import_key(private_key)
+    cipher_rsa = PKCS1_OAEP.new(key)
+    decrypted_message = cipher_rsa.decrypt(encrypted_message)
+    return decrypted_message
 
-def generate_shared_secret(public_key, private_key, p):
-    return (public_key ** private_key) % p
+def main():
+    server_private_key, server_public_key = generate_key_pair()
+    client_private_key, client_public_key = generate_key_pair()
 
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('localhost', 12345))
+    server.listen(1)
+    print("Server listening...")
 
-HOST = '127.0.0.1'
-PORT = 8080
+    conn, addr = server.accept()
 
-sock = socket.socket()
-sock.bind((HOST, PORT))
-sock.listen(1)
-conn, addr = sock.accept()
+    conn.send(server_public_key)
+    client_public_key = conn.recv(4096)
 
-msg = conn.recv(1024)
-print(pickle.loads(msg))
+    message = "Hello, client!"
+    encrypted_message = encrypt_message(message.encode(), client_public_key)
+    conn.send(encrypted_message)
 
-conn.close()
+    received_message = conn.recv(4096)
+    decrypted_message = decrypt_message(received_message, server_private_key)
+    print("Received message from client:", decrypted_message.decode())
+
+    conn.close()
+    server.close()
+
+if __name__ == "__main__":
+    main()
