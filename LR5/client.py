eb6bc89afe113dadddcd965dcf47231922efb39d
unknown <misschicherina@gmail.com>
2024-05-17 17:01:47 +0300

    Добавлены функции генерации ключей

--- LR5/client.py
+++ LR5/client.py
@@ -1,5 +1,16 @@
 import socket
 import pickle
+import random
+
+def generate_private_key():
+    return random.randint(2, 100)
+
+def generate_public_key(g, p, private_key):
+    return (g ** private_key) % p
+
+def generate_shared_secret(public_key, private_key, p):
+    return (public_key ** private_key) % p
+
 
 HOST = '127.0.0.1'
 PORT = 8080
